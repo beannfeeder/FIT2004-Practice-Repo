7321350d8473c85aa2c504e57794490945dc8378
Ng Yan Bin <yanbinngg027@gmail.com>
2025-03-22 22:57:14 +0800

    modified:   RadixSort.py

--- RadixSort.py
+++ RadixSort.py
@@ -17,27 +17,34 @@ def RadixSortNum(new_list):
     #finished finding max_column, now do the operation
     for column in range(max_column,0,-1):
         #now, compare every element in new_list
+        count_array = []
+        for _ in range(base):
+            count_array[_]=[]
+
         for element in (new_list):
             #skips the number if it doesnt have the digit
             if len(element)< column:
                 continue
             #finds val to put inside count array
-            val = element[column]
-            key = (val // (base^column))% base
-            #after we get the key, put the item inside count-array
-            #instantiate count-array
-            count_array = [None] * base
+            key = (element // (base**column))% base
+            #after we get the key, append the item inside count-array
+            count_array[key].append(element)
+            
+            #copied from countingsort
+            index = 0
             for i in range(len(count_array)):
-                count_array[i] =[]
-            print("Count-Array:", count_array)
+                item = i
+                frequency = count_array[i]
+                for j in range(len(frequency)):
+                    new_list[index] = item
+                    index += 1
+
+    
+    return new_list
 
 def RadixSortAlpha(new_list):
     column_counter = 0
 
-def __main__():
-    # Test case for RadixSortNum
-    test_numbers = [3, 4, 100, 50, 2000, 642, 30945]
-    print("Original list:", test_numbers)
-    sorted_numbers = RadixSortNum(test_numbers)
+print(example_num)
 
 
