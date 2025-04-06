df7d6339b9b128d535946877146b9f7d12254c0e
Ng Yan Bin <yanbinngg027@gmail.com>
2025-04-07 00:12:28 +0800

    new file:   Skeleton Dijkstra Python.py

---
+++ Skeleton Dijkstra Python.py
@@ -0,0 +1,29 @@
+class MinHeap:
+    """
+    Code all your minheap stuffs here
+    I only want to see the following 4 functions in the code review. You can have more in your own code.
+    The arguments, naming etc are badly done for a reason. Rename / redo it to something suitable.
+    """
+    def __int__(self, size):
+        something = [None] * size   # this is an array. Don't use ArrayR or anything.
+        pass
+    def rise(self, x):
+        pass
+    def sink(self, x):
+        pass
+    def update(self, x, y):
+        """
+        This is where you want to have the heap update. This is needed.
+        Make sure update() is O(log n) time max and not O(n).
+        """
+        pass
+
+class Graph:
+    """
+    I only want to see Dijkstra here. You can have more in your own code.
+    """
+    def dijkstra(self, x):
+        """
+        This is where you want to implement the dijkstra algorithm.
+        """
+        pass
