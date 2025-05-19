46fbff9b7bbbe857452288973f6b2db8394912cd
Ng Yan Bin <yanbinngg027@gmail.com>
2025-05-19 12:23:22 +0800

    finished trie insert and search 	modified:   Trie.py

--- Trie.py
+++ Trie.py
@@ -24,17 +24,58 @@ class Trie:
         """
         iterative implementation
         """
+        current = self.root
         for char in key:
             print(char)
+            actual_key = ord(char) - 97 + 1 #97 for starting small alphabet, 1 for leaving space for terminal char
+            if current.links[actual_key]: #there is a path, move current to that path
+                current = current.links[actual_key]
+
+            else: #no path, create one out and move current
+                print("making new node")
+                current.links[actual_key] = Node()
+                current = current.links[actual_key]
+        #finished adding char nodes, now end with terminal char $
+        # we know we left out node.links[0] as the terminal char section
+        terminal_key = 0
+        if current.links[terminal_key]: # there is a previous version of the word, we are just updating the data
+            print("magic!")
+            current = current.links[terminal_key]
             
+        else: #make a end node
+            current.links[terminal_key]=Node()
+            current = current.links[terminal_key]
+
+        current.data = data
+        print(data)
     
-    def search(self):
-        pass
+    def search(self, key):
+        """
+        traverse through nodes and return the data of the given input key
+        """
+        current = self.root
+        for char in key:
+            print(f"searching {char}")
+            actual_key = ord(char) - 97 + 1 #97 for starting small alphabet, 1 for leaving space for terminal char
+            if current.links[actual_key]: #there is a path, move current to that path
+                current = current.links[actual_key]
+
+            else: #no path, create one out and move current
+                raise(KeyError, "no key found")
+        terminal_key = 0
+        if current.links[terminal_key]:
+            current = current.links[terminal_key]
+            return current.data
+        else:
+            raise(KeyError, "some other error lol")
+
+
 
 #example
-the_strings = [["apple", 1024],["app", 64],["appendix",234],["lol", 69]]
+the_strings = [["apple", 1024],["app", 64],["appendix",234],["lol", 69], ["apple",6789]]
 
 example_trie = Trie()
 for pair in the_strings:
     example_trie.insert(pair[0],pair[1])
-
+print(example_trie.search("apple"))
+print(example_trie.search("pie"))
